e2e9df9a32201f5c12fca48196c5782de5427f2d
= <=>
2018-02-25 20:37:31 +0200

    added augmentation in the train

--- 3-train.py
+++ 3-train.py
@@ -12,7 +12,12 @@ X = h5f['X'] #images
 Y = h5f['Y'] #labels
 X = np.reshape(X, (-1, 48, 48, 1))
 
-network = input_data(shape=[None, 48, 48, 1]) #48 x 48 grayscale
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_crop((24, 24))
+img_aug.add_random_rotation(max_angle=25.)
+
+network = input_data(shape=[None, 48, 48, 1], data_augmentation=img_aug) #48 x 48 grayscale
 network = conv_2d(network, 64, 5, activation='relu')
 #network = local_response_normalization(network)
 network = max_pool_2d(network, 3, strides=2)
@@ -33,6 +38,6 @@ model = tflearn.DNN(network, checkpoint_path='check/',
 
 model.fit(X, Y, n_epoch=80, validation_set=0.15, shuffle=True,
           show_metric=True, batch_size=100,
-          snapshot_epoch=True, run_id='test')
+          snapshot_epoch=True, run_id='test_augmentation')
 
 model.save("model.tfl")
